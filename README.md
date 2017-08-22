eb4f7a124a00ce6db3c1cc851bd60a3a991f8a86
Manuel Ochoa <manuel.ochoa@aliadosweb.com>
2017-08-22 10:59:26 -0500

    First deployment

---
+++ README.md
@@ -0,0 +1,32 @@
+# Solr Ajax Implementation
+
+## Installation
+
+You must have _Browserify_ installed globaly
+
+`npm install -g browserify`
+
+`git clone https://github.com/duvanbedoya/Solr-Web-implementation` this repo
+
+`cd Solr-Web-implementation/`
+
+`npm install`
+
+
+
+## Deploy 
+
+### Building
+
+`npm run build`
+
+> open index.html
+
+### Run 
+
+`npm start`
+
+
+### OR
+
+Use _http-serve_ and serve it locally
---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+(function () {
+
+    /*var $ = require('jquery');*/
+
+    $("#warning").hide();
+    var solr = require('solr-client');
+
+    var client = solr.createClient('192.168.30.67', '8983', 'wiki');
+    console.log(client);
+    $('#ping').click(function () {
+        client.ping(function (err, obj) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(obj);
+                $("#status").html(obj.status);
+            }
+        });
+    });
+    $('#query').click(function () {
+        if ($('#queryText').val() !== '') {
+            var query = client.createQuery()
+                .q($('#queryText').val())
+                .start(0)
+                .rows(10);
+            client.search(query, function (err, obj) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(obj);
+                    $("#list-results").html("");
+                    $("#warning").hide();
+
+                    var newHTML = [];
+                    if (obj.response.docs.length > 0) {
+                        for (var i = 0; i < obj.response.docs.length; i++) {
+                            newHTML.push('<li class="list-group-item">' + obj.response.docs[i].id + '</li>');
+                        }
+                        $("#list-results").html(newHTML.join(""));
+
+                    } else {
+                        $("#warning").show();
+                    }
+
+                }
+            });
+        }
+        else {
+            alert("Empty field");
+        }
+        $('#queryText').val("");
+
+    });
+
+
+})();
+
+
+
